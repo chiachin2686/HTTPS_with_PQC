16f914a0e5852f581447dec70c172119ba0d7ad4
chiachin2686 <chiachin112686@gmail.com>
2022-05-22 18:44:27 +0800

    Update some files

--- AWS_Server_HTTPS_PQC/check_file.sh
+++ AWS_Server_HTTPS_PQC/check_file.sh
@@ -15,22 +15,36 @@ function file_changed(){
     done
 }
 
-function file_existed(){
+function file_checked(){
     local file_path="/opt/httpd/logs/ssl_request_log"
-    local check_time=60
+    local check_time=30
+    counter=0  # set a counter
     while [[ true ]]; do
-        if [ -f ${file_path} ]; then  # file exists
-            echo "${file_path} exists."
-            grep GET ${file_path} | tail -1 | awk '{print $7}' > /db/data/sensor_data.txt  # get the last data
-        else
-            echo "${file_path} does not exists."
-        fi
+        file_old_stat="`stat ${file_path}|grep Modify`"
         sleep ${check_time}
+        file_new_stat="`stat ${file_path}|grep Modify`"
+        if [ -f ${file_path} ]; then  # file exists
+            if [[ `echo ${file_old_stat}` == `echo ${file_new_stat}` ]]; then
+                echo "### File doesn't change ###"
+            else
+                grep GET ${file_path} | tail -1 | awk '{print $7}' > /db/data/sensor_data.txt  # get the last data
+                if [[ "$counter" -gt 1000 ]]; then
+                    sed -i '1,1000d' ${file_path}
+                    counter=0
+                else
+                    counter=$((counter+1))
+                fi
+                echo "### File changes ###"
+            fi
+            echo "File exists."
+        else
+            echo "File does not exists."
+        fi
     done
 }
 
 touch /db/data/sensor_data.txt
 
-file_existed
+file_checked
 
 ## use '&' to execute script in background, e.g., `sh check_file.sh &`
